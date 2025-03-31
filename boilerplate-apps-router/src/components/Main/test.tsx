63ee0790ced9ae87dcb2610ca56a450da991b224
Bispo <bispobispocruz@gmail.com>
2025-03-31 10:59:22 -0300

    improvements made without snapshots

--- boilerplate-apps-router/src/components/Main/test.tsx
+++ boilerplate-apps-router/src/components/Main/test.tsx
@@ -10,6 +10,14 @@ describe("<Main />", () => {
       screen.getByRole("heading", { name: /react avançado/i })
     ).toBeInTheDocument();
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it("should render the colors correctly", () => {
+    //remderoza o component
+    const { container } = render(<Main />);
+
+    //verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ "background-color": "#06092b" });
   });
 });
